f373f6a3a311659d7a8551ecdfa4f056a90dd3cb
natalialejandra1 <natalialeortiz@gmail.com>
2025-05-09 16:27:19 -0400

    Add files via upload

---
+++ index.hmtl.h
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Image Page 1</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
+</head>
+<body>
+  <div class="container-fluid p-0">
+    <a href="page2.html">
+      <img src="https://via.placeholder.com/1200x600" class="img-fluid w-100" alt="Clickable Image Page 1" />
+    </a>
+  </div>
+</body>
+</html>
